d86b8f1d84cfcd672c45a7fe0ab14f2e6b0c692a
MPL <manbobo2012@gmail.com>
2020-08-18 21:38:04 +0800

    update

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,21 +1,30 @@
-import React from 'react';
-import {View, Text, StyleSheet, Button, TextInput} from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, Button } from 'react-native';
 
 import Card from '../components/Card';
+import Input from '../components/Input';
+import Colors from '../constants/colors';
 
 const StartGameScreen = props => {
+
+    const [enteredValue, sestEnteredValue] = useState('');
+
+    const numberInputHandler = inputText => {
+        sestEnteredValue(inputText.replace());
+    };
+
     return (
         <View style={styles.screen}>
             <Text style={styles.title}>The Game Screen!</Text>
             <Card style={styles.inputContainer}>
                 <Text>Select a Number</Text>
-                <TextInput />
+                <Input style={styles.input} blurOnSubmit autoCapitalize='none' autoCorrect={false} keyboardType="number-pad" maxLength={2} onChangeText={numberInputHandler} value={enteredValue} />
                 <View style={styles.buttonContainer}>
                     <View style={styles.button}>
-                    <Button title="Reset" onPress={()=>{}} color="#c717fc"/>
+                        <Button title="Reset" onPress={() => { }} color={Colors.accent} />
                     </View>
                     <View style={styles.button}>
-                        <Button title="Confirm" onPress={()=>{}} color="#f7287b"/>
+                        <Button title="Confirm" onPress={() => { }} color={Colors.primary} />
                     </View>
                 </View>
             </Card>
@@ -23,7 +32,7 @@ const StartGameScreen = props => {
     )
 };
 
-const styles= StyleSheet.create({
+const styles = StyleSheet.create({
     screen: {
         flex: 1,
         padding: 10,
@@ -35,17 +44,21 @@ const styles= StyleSheet.create({
     },
     inputContainer: {
         width: 300,
-        maxWidth:'80%',
-        alignItems:'center'
+        maxWidth: '80%',
+        alignItems: 'center'
     },
     buttonContainer: {
-        flexDirection:'row',
+        flexDirection: 'row',
         width: '100%',
         justifyContent: 'space-between',
         paddingHorizontal: 15
     },
     button: {
         width: 100
+    },
+    input: {
+        width: 50,
+        textAlign: 'center'
     }
 });
 
